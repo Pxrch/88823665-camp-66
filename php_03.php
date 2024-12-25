c19bf628c37f3db4def9bb3d409972e1c4ce9852
Supanat <supanatheakarn@gmail.com>
2024-12-25 14:54:18 +0700

    เสร็จแล้ว

--- php_03.php
+++ php_03.php
@@ -5,10 +5,42 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <div class="container mt-5">
-        <h1>email : <?php if(isset($_POST['email'])){ echo $_POST['email'];} ?></h1>
-            
+        <form method="post" action="">
+            <div class="text-center">
+                <h1 class="display-4 fw-bold"> แม่สูตรคูณ </h1>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label display-6"> แม่สูตรคูณ </label>
+                <input name="mother" type="number" class="form-control" id="exampleFormControlInput1" placeholder="กรอกเลขแม่สูตรคูณ" required>
+            </div>
+            <div class="mb-3">
+                <button class="btn btn-success" type="submit">แปลงสูตรคูณ</button>
+            </div>
+
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $number = $_POST["mother"];
+                for ($i = 1; $i <= 12; $i++) {
+                    $total = $number * $i;
+            ?>
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between">
+                                <span class="h6"><?php echo $number ?> x <?php echo $i ?></span>
+                                <span class="h6 "> = </span>
+                                <span class="h6"><?php echo $total ?></span>
+                            </div>
+                        </div>
+                    </div>
+            <?php
+                }
+            }
+            ?>
+
+        </form>
     </div>
 </body>
 
