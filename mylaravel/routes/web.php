e4d34d9bf5bb307f24259b831bed1f98548b9d3f
Supanat <supanatheakarn@gmail.com>
2025-01-15 18:55:00 +0700

    laravel template

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
+use App\Http\Controllers\loginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\HomeController;
+
+Route::get('/login',
+    [LoginController::class, 'index']);
+
+Route::get('/register',
+    [RegisterController::class, 'regis']);
 
 Route::get('/mycontroller/{id?}',
     [MyController::class, 'myfunction']);
@@ -10,8 +19,11 @@ Route::post('/mycontroller/{id?}',
     [MyController::class, 'myfunction']);
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/home',
+    [HomeController::class, 'House']);
 
 
 Route::get('/hello{/id?}',
