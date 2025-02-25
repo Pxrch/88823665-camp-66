5d4ac9893d54369bcce1fc2c9beeee9fa21dbbf8
Supanat <supanatheakarn@gmail.com>
2025-02-25 20:22:26 +0700

    Login และ Dynamic Form

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -7,44 +7,30 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\CheckLogin;
 
-Route::get('/product', [ProductController::class, '']);
-Route::get('/login',
-    [LoginController::class, 'index']);
+Route::get("/product",[ProductController::class,"index"])->middleware([CheckLogin::class,]);
+Route::post("/product",[ProductController::class,"store"])->middleware([CheckLogin::class,]);
 
-Route::post('/login',
-    [loginController::class, 'login']);
+Route::get('/login',[LoginController::class, 'index']);
+Route::post('/login',[loginController::class, 'login']);
 
-Route::get('/register',
-    [RegisterController::class, 'regis']);
+Route::get('/register',[RegisterController::class, 'regis']);
+Route::post('/register',[RegisterController::class, 'create']);
 
-Route::post('/register',
-    [RegisterController::class, 'create']);
-
-Route::get('/users',
-    [UserController::class, 'index']);
-
-Route::get('/user/{id}',
-    [UserController::class, 'edit']);
-
-Route::put('user',
-    [UserController::class, 'edit_action']);
-
-Route::delete('user',
-    [UserController::class, 'delete']);
+Route::get('/users',[UserController::class, 'index']);
+Route::get('/user/{id}',[UserController::class, 'edit']);
+Route::put('user',[UserController::class, 'edit_action']);
+Route::delete('user',[UserController::class, 'delete']);
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/home',
-    [HomeController::class, 'House']);
+Route::get('/home',[HomeController::class, 'House']);
 
-Route::get('/mycontroller/{id?}',
-    [MyController::class, 'myfunction']);
-
-Route::post('/mycontroller/{id?}',
-    [MyController::class, 'myfunction']);
+Route::get('/mycontroller/{id?}',[MyController::class, 'myfunction']);
+Route::post('/mycontroller/{id?}',[MyController::class, 'myfunction']);
 
 Route::get('/hello{/id?}',
     function ($val =""){
